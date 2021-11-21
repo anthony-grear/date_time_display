d6f488eaac9ebaf0330f06ceabd0321d6db484f6
Anthony Grear <tgrear2008@gmail.com>
2021-11-21 22:44:08 +0700

    Reorganize the format of the application. No change to output.

--- dateTime/src/dateTime/DateTime.java
+++ dateTime/src/dateTime/DateTime.java
@@ -3,16 +3,52 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class DateTime extends JFrame {
+
+public class DateTime {
+	
+	
+	//frame constructor
+	private static void createAndShowGUI() {
+		//create and set up window
+		JFrame frame = new JFrame("Date and Time");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300,190);
+		frame.setLocation(950,100);
+		
+		JLabel timeLabel = new JLabel();
+		frame.getContentPane().setLayout(new GridBagLayout());
+		//add controls
+		GridBagConstraints gridConstraints = new GridBagConstraints();
+		
+		timeLabel.setText("Hello World");
+		gridConstraints.gridx=0;
+		gridConstraints.gridy=0;
+		frame.getContentPane().add(timeLabel,gridConstraints);
+		
+		
+		frame.setVisible(true);
+	}
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-		new DateTime();
+		//Schedule a job for the event-dispatching thread
+		//creating and showing the application's GUI
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
 		
 	}
-	public DateTime() {
-		setSize(300,190);
-		setLocation(950,100);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setTitle("Current Date Time");
-		setVisible(true);
-	}
+	
+	
+	
+		
+	
+
+	
 }
