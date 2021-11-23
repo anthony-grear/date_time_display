93650fc280da400984e298cc7d760e57e5d2f088
Anthony Grear <tgrear2008@gmail.com>
2021-11-23 16:19:23 +0700

    Add Panel and Button. The Button outputs current date and time to the terminal

--- dateTime/src/dateTime/MainFrame.java
+++ dateTime/src/dateTime/MainFrame.java
@@ -1,19 +1,56 @@
 package dateTime;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
 
 public class MainFrame {
 	private JFrame frame;
+	private JPanel panel;
+	private JButton button;
+	private JLabel label;
 	
 	public MainFrame() {
 		initialize();
 	}
 	
 	public void initialize() {
-		frame = new JFrame();
+		frame = new JFrame();		
 		this.frame.setTitle("Date and Time");
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.frame.setSize(300,190);
 		this.frame.setLocation(950,100);
+		this.frame.setLayout(new BorderLayout());
+		
+		panel = new JPanel();
+		this.panel.setBackground(new Color(0,0,255));
+		this.panel.setSize(new Dimension(100, 30));
+		this.frame.add(panel, BorderLayout.WEST);
+		
+		button = new JButton();
+		this.button.setText("Current Time");
+		this.button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DateTimeFormatter dateTimeFormatObj = DateTimeFormatter.ofPattern("E, MMM dd yyyy HH:mm:ss");
+				LocalDateTime localDateTime = LocalDateTime.now();
+				String formattedDateTimeObj = localDateTime.format(dateTimeFormatObj);
+				
+				System.out.println(formattedDateTimeObj);
+			}
+			
+		});
+		this.panel.add(button);
+		
+		
+		
 		this.frame.setVisible(true);
 	}
 }
